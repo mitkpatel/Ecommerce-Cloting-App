f5d9d4733c7469ba8dab114ae5bb803a4f43e89c
Mit Patel <mitkpatel09@gmail.com>
2022-06-07 23:15:23 -0400

    add to cart-icon and menu

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -11,9 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+        <UserProvider>
+          <ProductsProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </ProductsProvider>
+        </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
